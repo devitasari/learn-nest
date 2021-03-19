8cd9038fc02c89331d3061d8217acd0484a994df
devita <devitas700@gmail.com>
2021-03-19 19:00:41 +0700

    remove node modules

--- dist/aws/s3/s3.service.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class S3Service {
-    private readonly configService;
-    private readonly s3;
-    private readonly bucket;
-    constructor(configService: any);
-}
--- dist/config/app.config.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const AWS_ACCESS_KEY_ID: string;
-export declare const AWS_SECRET_ACCESS_KEY: string;
-export declare const AWS_REGION: string;
-export declare const AWS_BUCKET: string;
